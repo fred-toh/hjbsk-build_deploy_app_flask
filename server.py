f63a38e1f0ed37868cca18a4f3b17595d6c34220
fred-toh <fredtoh@hotmail.com>
2024-05-27 06:29:45 -0400

    Changed index.html and server.py

--- server.py
+++ server.py
@@ -1,37 +1,40 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, redirect, url_for
 # Import the Maths package here
 from Maths.mathematics import summation, subtraction, multiplication
 
 app = Flask("Mathematics Problem Solver")
 
-@app.route("/sum")
-def sum_route():
-    num1 = float(request.args.get('num1'))
-    num2 = float(request.args.get('num2'))
-    # Write your code here
-    result = summation(num1, num2)
-    return str(result)
+result = {'result':""}
 
-@app.route("/sub")
-def sub_route():
-    num1 = float(request.args.get('num1'))
-    num2 = float(request.args.get('num2'))
+@app.route("/sum", methods=["POST"])
+def sum_route(num1, num2):
+    # num1 = float(request.args.get('num1'))
+    # num2 = float(request.args.get('num2'))
     # Write your code here
-    result = subtraction(num1, num2)
-    return str(result)
+    result['result'] = str(summation(num1, num2))
+    return redirect(url_for("render_index_page"))
 
-@app.route("/mul")
-def mul_route():
-    num1 = float(request.args.get('num1'))
-    num2 = float(request.args.get('num2'))
+@app.route("/sub", methods=["POST"])
+def sub_route(num1, num2):
+    # num1 = float(request.args.get('num1'))
+    # num2 = float(request.args.get('num2'))
+    # Write your code here
+    result['result'] = str(subtraction(num1, num2))
+    return redirect(url_for("render_index_page"))
+
+@app.route("/mul", methods=["POST"])
+def mul_route(num1, num2):
+    # num1 = float(request.args.get('num1'))
+    # num2 = float(request.args.get('num2'))
     # Write your code here  
-    result = multiplication(num1, num2)
-    return str(result)
+    result['result'] = str(multiplication(num1, num2))
+    return redirect(url_for("render_index_page"))
+
 
 @app.route("/")
 def render_index_page():
     # Write your code here
-    return render_template("index.html")
+    return render_template("index.html", result=result['result'])
 
 @app.errorhandler(422)
 def invalid_input(error):
